0f72a353b1b1c8dd1c2680d279e0bf8e68ff08e3
Isaac Sparling <isaac.sparling@gmail.com>
2014-08-15 11:53:41 -0700

    looks like controller needs to be defined in the gem

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,6 +3,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def stripe_connect
     # Delete the code inside of this method and write your own.
     # The code below is to show you where to access the data.
+    
+    binding.pry
+    puts 'hi'
     raise request.env["omniauth.auth"].to_yaml
   end
 
